0d089dbd4e569fc8938bb02fc036bc46af728360
Olga Bieliaieva <belyaevaolya0@gmail.com>
2023-05-15 20:59:21 +0300

    copied start files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import SectionTitle from '../SectionTitle/SectionTitle';
+import ContactForm from '../ContactForm/ContactForm';
+import Filter from '../Filter/Filter';
+import ContactList from '../ContactList/ContactList';
+import css from './App.module.css';
+
+export default function App() {
+  return (
+    <main className={css.appContainer}>
+      <SectionTitle text="Phonebook" />
+      <ContactForm />
+      <SectionTitle text="Contacts" />
+      <Filter />
+      <ContactList />
+    </main>
+  );
+}
---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,50 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { remove, getContacts } from 'redux/contactsSlice';
+import { getFilterQuery } from 'redux/filterSlice';
+
+import css from './ContactList.module.css';
+
+const ContactList = () => {
+  const contacts = useSelector(getContacts);
+  const filterQuery = useSelector(getFilterQuery);
+  const dispatch = useDispatch();
+
+  const removeContact = e => {
+    const contactId = e.target.id;
+    const remainingContacts = contacts.filter(
+      contact => contact.id !== contactId
+    );
+    dispatch(remove(remainingContacts));
+  };
+
+  return contacts.length > 0 ? (
+    <ul className={css.contactList}>
+      {contacts
+        .filter(contact =>
+          contact.name.toLowerCase().includes(filterQuery.toLowerCase())
+        )
+        .map(({ id, name, number }) => {
+          return (
+            <li className={css.contactItem} key={id}>
+              {name}: {number}
+              <button
+                className={css.listItemBtn}
+                id={id}
+                type="button"
+                onClick={removeContact}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+    </ul>
+  ) : (
+    <p className={css.contactListDefault}>
+      Sorry, your phonebook is empty. <br></br>
+      Add your first contact, please.
+    </p>
+  );
+};
+
+export default ContactList;
